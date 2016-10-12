2762b2fde6bff69a3540e2ee41cdc945f80ac636
cnobles <cnobles815@gmail.com>
2016-10-12 17:16:47 -0400

    More tests. Deck complete.

--- src/test/java/christopher/nobles/casino/DeckTEST.java
+++ src/test/java/christopher/nobles/casino/DeckTEST.java
@@ -8,6 +8,16 @@ import static org.junit.Assert.assertEquals;
  * Created by christophernobles on 10/12/16.
  */
 public class DeckTEST {
+
+    @Test
+    public void deckLengthTest(){
+        Deck deck = new Deck();
+
+        int expected = 52;
+        int actual = deck.standardDeck.length;
+        assertEquals("The value should be 52" , expected, actual);
+    }
+
     @Test
     public void clubAceTest(){
         Deck deck = new Deck();
@@ -77,7 +87,50 @@ public class DeckTEST {
         Suit expectedSuit = Suit.HEARTS;
         Suit actualSuit = ace.getSuit();
         assertEquals("The suit should be HEARTS" , expectedSuit, actualSuit);
-
     }
+
+    @Test
+    public void heartsKingTest(){
+        Deck deck = new Deck();
+
+        Card king = deck.standardDeck[38];
+        int expectedId = 13;
+        int actualId = king.getId();
+        assertEquals("The value should be 13", expectedId, actualId);
+
+        Suit expectedSuit = Suit.HEARTS;
+        Suit actualSuit = king.getSuit();
+        assertEquals("The suit should be HEARTS" , expectedSuit, actualSuit);
+    }
+
+    @Test
+    public void spadesAceTest(){
+        Deck deck = new Deck();
+
+        Card ace = deck.standardDeck[39];
+        int expectedId = 1;
+        int actualId = ace.getId();
+        assertEquals("The value should be 1", expectedId, actualId);
+
+        Suit expectedSuit = Suit.SPADES;
+        Suit actualSuit = ace.getSuit();
+        assertEquals("The suit should be SPADES", expectedSuit, actualSuit);
+    }
+
+    @Test
+    public void spadesKingTest(){
+        Deck deck = new Deck();
+
+        Card king = deck.standardDeck[51];
+        int expectedId = 13;
+        int actualId = king.getId();
+        assertEquals("The value should be 13", expectedId, actualId );
+
+        Suit expectedSuit = Suit.SPADES;
+        Suit actualSuit = king.getSuit();
+        assertEquals("The suit should be SPADES", expectedSuit, actualSuit);
+    }
+
+
 
 }
